a78e965bb1121430ba975ed116f89dc06ac8a1dc
nguye <nguye@LAPTOP-1HT5S1T7>
2021-09-29 07:52:43 +0700

    Add project files.

---
+++ Bài học/Program.cs
@@ -0,0 +1,24 @@
+﻿using System
+
+namespace Bài_học
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Endcoding.Unicode
+            Console.InputEncoding = Endcoding.Unicode
+            string chuỗi = "Vac xin tiem som nhat tot nhat"
+            Console.WriteLine(chuỗi)
+            string[] Taphopchuoi = chuỗi.Split(' ')
+            foreach(string S in Taphopchuoi)
+            {
+                Console.WriteLine(S)
+            }
+            Console.WriteLine(chuỗi.Contains("tiêm"))
+            Console.WriteLine(chuỗi.EndsWith("nhất"))
+            Console.WriteLine(chuỗi.Trim())
+            Console.ReadLine()
+        }
+    }
+}
